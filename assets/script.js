ce6b4101038d581b81ce43f1f537fb1dd5d6a0b2
Dexter Zacour <cdzacour@hotmail.com>
2022-06-05 12:31:25 -0400

    added time blocks

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,32 @@
 var todaysDate = document.querySelector("#currentDay")
-
+var container = document.querySelector(".container")
 
 var grabDate = function() {
 todaysDate.textContent = new Date().toString();
 }
 
-setInterval(grabDate)
+setInterval(grabDate);
+
+function createTimeBlock() {
+    for (var i = 9; i <= 17; i++) {
+        var currentHour = i <= 12 ? i : i - 12;
+
+        var row = document.createElement('div');
+        $(row).addClass("row time-block");
+
+        var hourBox = document.createElement('div');
+        $(hourBox).addClass("col-1 hour").text(currentHour);
+
+        var taskBox = document.createElement("textarea");
+        $(taskBox).addClass("col-10 description");
+
+        var saveBtn = document.createElement("button");
+        $(saveBtn).addClass("col-1 saveBtn").text("Save");
+
+        row.appendChild(hourBox);
+        row.appendChild(taskBox);
+        row.appendChild(saveBtn);
+        container.appendChild(row);
+    };
+};
+createTimeBlock();
