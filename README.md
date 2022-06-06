267d238a713c520e70845ec2946aa6602efb6428
Dexter Zacour <cdzacour@hotmail.com>
2022-06-06 19:27:18 -0400

    changed rows to be created through html, and finished up save and load with some tweaks to classDetermination

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ X THEN the current day is displayed at the top of the calendar
 WHEN I scroll down
 X THEN I am presented with time blocks for standard business hours
 WHEN I view the time blocks for that day
-THEN each time block is color-coded to indicate whether it is in the past, present, or future
+X THEN each time block is color-coded to indicate whether it is in the past, present, or future
 WHEN I click into a time block
-THEN I can enter an event
+X THEN I can enter an event
 WHEN I click the save button for that time block
 THEN the text for that event is saved in local storage
 WHEN I refresh the page
--- assets/script.js
+++ assets/script.js
@@ -1,62 +1,83 @@
 var todaysDate = document.querySelector("#currentDay");
-var container = document.querySelector(".container");
+var timeBlock = $(".time-block");
 
 
-var grabDate = function() {
-todaysDate.textContent = new Date().toString();
-var currentHour = Number(new Date().toString().split(" ")[4].split(":")[0]);
-return currentHour;
-}
+// display date and grab the value of the hour
+var grabDate = function () {
+  todaysDate.textContent = moment().format("dddd, MMMM Do YYYY, h:mm");
+  var currentHour = moment().format("H");
 
+  return currentHour;
+};
+// update the date every second
 setInterval(grabDate, 1000);
 
-function createTimeBlock() {
-    determineClass();
-    for (var i = 9; i <= 17; i++) {
+// load tasks when page loads
+var loadTasks = function () {
+    // set text'#' variables equal to the corosponding 'key' (textarea id)
+    var text9 = localStorage.getItem(9);
+    var text10 = localStorage.getItem(10);
+    var text11 = localStorage.getItem(11);
+    var text12 = localStorage.getItem(12);
+    var text13 = localStorage.getItem(13);
+    var text14 = localStorage.getItem(14);
+    var text15 = localStorage.getItem(15);
+    var text16 = localStorage.getItem(16);
+    var text17 = localStorage.getItem(17);
+  
+    // reset textarea by using id's and setting their values to the declared vairables
+    $("#9").val(text9);
+    $("#10").val(text10);
+    $("#11").val(text11);
+    $("#12").val(text12);
+    $("#13").val(text13);
+    $("#14").val(text14);
+    $("#15").val(text15);
+    $("#16").val(text16);
+    $("#17").val(text17);
+
+
+
+    // loop through time blocks
+    for (i = 9; i <= 17; i++) {
         var hour = i;
+        // add a class to the textarea's based on the result of the 'determineClass' function
+        $(".description").addClass(`${determineClass(grabDate(), i)}`);
+      }
+    
+  };
 
-        var row = document.createElement('div');
-        row.classList.add("time-block");
-        row.classList.add("row")
 
-        var hourBox = document.createElement('div');
-        hourBox.classList.add("hour");
-        hourBox.classList.add("col-1")
-        hourBox.textContent = hour;
 
-        var taskBox = document.createElement("textarea");
-        taskBox.classList.add(`${determineClass(grabDate(), i)}`);
-        taskBox.classList.add("col-10")
-        taskBox.classList.add("description")
-
-        var saveBtn = document.createElement("button");
-        saveBtn.classList.add("saveBtn");
-        saveBtn.classList.add("col-1");
-        saveBtn.textContent = "Save";
-        saveBtn.addEventListener("click", saveTask());
-
-        row.appendChild(hourBox);
-        row.appendChild(taskBox);
-        row.appendChild(saveBtn);
-        container.appendChild(row);
-    };
-};
-
+// function to determine class of textarea's
 function determineClass(currentHour, hour) {
-   
-    if (hour < currentHour) {
-        return "past"
-    }
-    if (hour === currentHour) {
-        return "present"
-    }
-    if (currentHour < hour) {
-        return "future"
-    }
+    console.log(currentHour, hour)
+
+    $(".description").removeClass("past present future");
+
+  // if the hour of the text area is less than the current hour in the day then give it a class of "past"
+  if (hour < currentHour) {
+    return "past";
+  }
+  // if the hour of the textarea is equal to the current hour in the day, set the class to "present"
+  if (hour == currentHour) {
+    return "present";
+  }
+  // if the current hour of the day is less than the hour of the text area, set the class to "future"
+  if (currentHour < hour) {
+    return "future";
+  }
 }
 
-var saveTask = function() {
-    console.log("save")
-}
+// on click of the dave button in a particular row
+$(".row").on("click", ".saveBtn", function () {
+  // set the variable task list to the value of whats in the child textarea
+  var tasktext = $(this).parent().find(".description").val().trim();
+  // set the value of time equal to the id of the child text area
+  var time = $(this).parent().find(".description").attr("id");
+  // save both to local storage
+  localStorage.setItem(time, tasktext);
+});
 
-createTimeBlock();
+// load tasks on page load
+loadTasks();
